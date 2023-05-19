2a9cbbe5f412de6b15ebb75d24c9dc375a22f625
Walid Chrif <103291201+WalidChrif@users.noreply.github.com>
2023-05-19 18:40:54 +0100

    rm just a check

--- src/main/java/com/mytest/MyTestApplication.java
+++ src/main/java/com/mytest/MyTestApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyTestApplication {
 
     public static void main(String[] args) {
-        //a check
         SpringApplication.run(MyTestApplication.class, args);
     }
 
